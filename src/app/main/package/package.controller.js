37ee46feb214b327121ce36342882a3107b80d56
sebastian.bogado <sebastian.j.bogado@gmail.com>
2015-11-10 03:22:56 -0300

    Package view model tied up

--- src/app/main/package/package.controller.js
+++ src/app/main/package/package.controller.js
@@ -6,8 +6,10 @@
     .controller('PackageController', PackageController);
 
   /** @ngInject */
-  function PackageController() {
+  function PackageController($scope, $stateParams) {
     var vm = this;
+
+    angular.extend(vm, $scope.main.packages[$stateParams.id]);
 
 
     activate();
